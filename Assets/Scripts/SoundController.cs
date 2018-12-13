79f29a071e8839af6cc80e79b64440edf5a05543
wcram <wcram@live.com>
2018-12-12 21:05:10 -0800

    Add Sounds folder. Edit SoundController

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    public AudioClip diceHit;
+    public AudioClip ground;
+
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+
+
+    //} // End Update()
+
+    public void OnTriggerEnter(Collider other)
+    {
+        
+
+
+    } // End OnTriggerEnter()
+
+} // End class SoundController
